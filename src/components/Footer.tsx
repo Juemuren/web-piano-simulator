bcbb09d04ce11570198b10615b20ec066ab496bb
Raind <127611616+Juemuren@users.noreply.github.com>
2026-04-27 17:29:37 +0800

    docs: 修改 README 和 Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -68,18 +68,18 @@ function Footer() {
             在乐谱编辑器中，单击乐谱中的音符可以选中并播放这个音符，而双击则会直接从选中处演奏整个乐谱
           </FooterPanel>
           <FooterPanel title="乐谱编码">
-            乐谱使用 ABC Notation 编码格式，详细的标准请阅读官方给出的学习资源
+            乐谱使用 ABC Notation 编码格式，详细的标准可阅读官方教程
           </FooterPanel>
           <FooterPanel title="运行原理">
-            钢琴声音基于 Web Audio
-            进行纯物理合成，数学原理和代码实现可以参考给出的链接
+            声音基于 Web Audio API
+            纯物理合成，数学原理和代码实现可分别参考我的科普文章与源代码
           </FooterPanel>
         </div>
 
         <div className="grid gap-3 sm:justify-items-end">
-          <FooterLink href={articleUrl} label="相关数学原理" />
+          <FooterLink href={articleUrl} label="数学原理科普文章" />
           <FooterLink href={repositoryUrl} label="源代码仓库" />
-          <FooterLink href={abcUrl} label="乐谱编码格式" />
+          <FooterLink href={abcUrl} label="乐谱格式教程" />
         </div>
       </div>
     </footer>
